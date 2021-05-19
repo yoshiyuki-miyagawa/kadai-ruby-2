d0cec1f6f1376f41880e9973d3d818761fe063db
yoshiyuki.miya <akayoshi3844@yahoo.ne.jp>
2021-05-19 13:47:21 +0000

    kadai-ruby-2

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,20 @@
+def fizzbuzz(num)
+# 3で割り切れるかつ、5で割り切れるときの処理
+    if num % 3 == 0 && num % 5 == 0  
+        return "fizzbuzz"
+# 3の倍数のときの処理
+    elsif num % 3 == 0
+        return "fizz"
+# 5の倍数のときの処理
+    elsif num % 5 == 0
+        return "buzz"
+# それ以外のときの処理
+    else
+        return num
+    end
+end
+
+num_max = 100
+(1..num_max).each do |num|
+    puts fizzbuzz(num)
+end
